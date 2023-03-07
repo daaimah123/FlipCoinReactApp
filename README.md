54ab554b099dcae295919ce37cd06cc5e08853fd
Owner <owner@Owners-MacBook-Pro.local>
2023-03-07 09:55:40 -0800

    set up flip coin and planet api

--- README.md
+++ README.md
@@ -1,68 +1,32 @@
+
+![Heads & Tails to Image](FlipCoin.gif)
+
+
+---------------
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+## HTML, REACT
 
-In the project directory, you can run:
+In the project directory, my objective was to create an app that was able to render a button that allows the user to toggle between the word heads and tails.
 
-### `npm start`
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+### Thought Process
+1. Create a clickable button.
+2. Output => toggle a displayed word head or tail (each with 50% probability)
+3. Create a start display state with null / nothing    
+4. Handle a click event that changes display (true => image toggle) state when click is on    
+5. Do not change display (false => stays as last state) with not clicked    
+6. Create a function that goes inside click event that handles toggle between heads and tails 
+7. Button does not disable
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+### Progression
 
-### `npm test`
+Was able to load results to console, not to page. 
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+![Screen Shot 2019-03-26 at 3 10 50 PM](https://user-images.githubusercontent.com/41805952/55126714-1a386f00-50cb-11e9-8853-f16669d35e7c.png)
 
-### `npm run build`
+Attempted use of `document.getElementById` to force text rendering to page. Toggle successful.
 
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
+![Screen Shot 2019-03-26 at 3 34 05 PM](https://user-images.githubusercontent.com/41805952/55126784-566bcf80-50cb-11e9-9a98-9b250b4ea13f.png)
 
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
--- src/App.js
+++ src/App.js
@@ -1,42 +1,104 @@
 import React, { Component } from 'react';
 import './App.css';
+// var request = require('request');
+
+
+// var FetchStream = require("fetch").FetchStream;
+
+// import coin from './coin.svg';
+
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //       isLoading: false, 
-  //       quarter: 'heads',
-  //       // url: google.com,//pic of image head
-  //   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      coinPic: '',
+      display: 'Click Me To Show Heads or Tails',
+      external: {}, //empty obj to state that there will be obj data eventually
 
-  // }
+    }
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.starWarsAPI = this.starWarsAPI.bind(this);
+    this.coinToss = this.coinToss.bind(this);
 
-  // //FIXME: make sure quarter populates to random heads or tails
-  // coinToss () {
-  //   // This function will randomly return either 'heads' or 'tails'.
-  //   return Math.random() < 0.5 ? 'heads' : 'tails';
-  //   this.setState = {
-  //     isLoading: true,
-  //     quarter: 'tails'
-  //   }
-  // }
+  }
 
-  //TODO: need to drop this inside a function that can be referenced by an onClick event for flip button
-  // // if/else statement begins here:
-  // if (coinToss() == "heads") {
-  //   var img = <img src={pics.kitty} />;
-  // } else {
-  //   var img = <img src={pics.doggy} />;
-  // }
+  componentDidMount() {
+    this.setState ({
+      coin: this.coinToss,
+      value: ''
+    })
+  }
 
+  //will handle input text from textbox
+  handleTextChange(input) {
+    this.setState(
+      {value: input.target.value} //id number typed into value box 
+      );
+  }
+
+  starWarsAPI(){//id established for when input is saved and used to return id planet
+    fetch(`https://swapi.co/api/planets/${this.state.value}`)
+    .then(res => res.json())
+    .then(body => {
+      this.setState({external: body});
+    console.log({body})
+    })
+    // request(`https://swapi.co/api/planets/${this.state.value}`, function (error, response, body) {
+    //   console.log('error:', error); // Print the error if one occurred
+    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //   console.log('body:', body); // Print the HTML from request.
+    //   //FIXME: currently printing out full body instead of specific information
+    //   document.getElementById('starWars').innerHTML=body; //SUCCESS: Printing to page as expected
+    //   console.log(`this: ${this}`)
+    //   this.setState({external: body})
+      
+    // });
+  }
+
+
+//TODO: change head and tail string into words
+// head image
+    // const headsImage = new Image(100, 200);
+    // headsImage.src = './us-quarter-front.jpg';
+    // const heads = document.body.appendChild(headsImage);
+    // // tail image
+    // var tailImage = new Image(100, 200);
+    // tailImage.src = './us-quarter-back.jpg';
+    // const tails = document.body.appendChild(tailImage);
+  coinToss(){
+    let coin = ['heads', 'tails']
+    let toss = Math.floor(Math.random() * 2) //randomly access indexes 0 to 1
+    // console.log(toss)//printing index randomly as expected to console
+    //printing coin sides randomly to console as expected
+    this.setState({
+      display: 'Click again to see what you get!'
+    })
+    if (coin[toss] === 'tails'){
+      this.setState({coinPic: './us-quarter-back.jpg', alt: 'back of US quarter'})
+    } else {
+      this.setState({coinPic: './us-quarter-front.jpg', alt: 'front of US quarter'})
+    }
+
+
+    // return document.getElementById('resultText').innerHTML=coin[toss]
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        {/* <p>quarter needs to be according to state</p>
-          <button onClick={this.coinToss}>Flip Coin</button> */}
+        <p>{this.state.display}</p>
+        <p id='resultText'>{this.setState.coin}</p>
+        <img src={this.state.coinPic} height={100} width={100} alt={this.state.alt}/>
+          <button onClick={this.coinToss}>Flip Coin</button> 
+          {/* ref={input => # = input} */}
+        <hr/><br/><br/><br/>
+          <form>
+              <textarea placeholder="Enter A Planet Number" value={this.state.value} onChange={this.handleTextChange} />
+          </form>
+          <p id='starWars'>Planet Name: {this.state.external.name}<br/>Climate: {this.state.external.climate} <br/>Terrain: {this.state.external.terrain}</p>
+          <button onClick={()=>this.starWarsAPI()}>Get Planet Details</button>
         </header>
       </div>
     );
